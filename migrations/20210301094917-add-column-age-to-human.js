241287fa860e1457d05078e49fe0e049ed60561e
David Pestana <david.pestana.perdomo@gmail.com>
2021-03-01 12:40:53 +0100

    seeder

---
+++ migrations/20210301094917-add-column-age-to-human.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.addColumn('Humans','age',{
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.removeColumn('Humans','age');
+  }
+};
---
+++ seeders/20210301112326-add-sample-human.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.bulkInsert('Humans', [
+      {
+        name: 'Mario',
+        genre:'H',
+        age: 12
+      },{
+        name: 'Lucia',
+        genre:'M',
+        age: 16
+      }
+  
+  ], {});
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
